444bebb164565dc53786ff2de635977ec9d6e506
mansori <mansorihack@gmail.com>
2023-04-18 22:23:32 +0700

    penambahan test check box

--- cypress/e2e/0-Personal-learn/interactingtest.cy.js
+++ cypress/e2e/0-Personal-learn/interactingtest.cy.js
@@ -1,8 +1,30 @@
 /// <reference types="cypress" />
 
-describe('Browser action', () => {
-  it('Should load books website', () => {
-    cy.visit("https://books.toscrape.com/index.html").wait(7000);
-    cy.url().should("include", "index.html")
-  })
-})
+describe('Working with inputs', () => {
+  it('Visit the website', () => {
+    cy.visit("http://zero.webappsecurity.com/login.html")
+    cy.url().should("include", "login.html")
+  });
+  it('Should fill username', () => {
+    cy.get('#user_login').clear()
+    cy.get('#user_login').type('username')
+  });
+  it('Should fill password', () => {
+    cy.get('input[name="user_password"]').clear()
+    cy.get('input[name="user_password"]').type('password')
+  });
+
+// Test
+it('should check the checkbox', () => {
+  cy.get('#user_remember_me').check().should('be.checked')
+});
+
+it('should uncheck the checkbox', () => {
+  cy.get('#user_remember_me').uncheck().should('not.be.checked')
+});
+
+  it('should toggle the checkbox', () => {
+    cy.get('#user_remember_me').click().should('be.checked')
+    cy.get('#user_remember_me').click().should('not.be.checked')
+  });
+});
