0df82b59d7f56a159e0fe7ff27c7927a16b59f4e
mansori <mansorihack@gmail.com>
2023-04-22 23:29:27 +0700

    Menambah input submit dan alert error

---
+++ cypress/e2e/0-Personal-learn/logintest.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Working with inputs', () => {
+    it('Visit the website', () => {
+      cy.visit("http://zero.webappsecurity.com/login.html")
+    });
+    it('Should fill username', () => {
+      cy.get('#user_login').clear()
+      cy.get('#user_login').type('username')
+    });
+    it('Should fill password', () => {
+      cy.get('input[name="user_password"]').clear()
+      cy.get('input[name="user_password"]').type('password')
+    });
+
+    it('Should try to login', () => {
+      cy.fixture("user").then(user => { 
+        const username = user.username
+        const password = user.password
+
+        cy.get('#user_login').clear()
+        cy.get('#user_login').type(username)
+
+        cy.get('input[name="user_password"]').clear()
+        cy.get('input[name="user_password"]').type(password)
+
+        cy.get('input[name="submit"]').click()
+
+        cy.get('.alert-error').should('contain.text', 'Login and/or password are wrong.')
+      })
+    });
+  });
+  
