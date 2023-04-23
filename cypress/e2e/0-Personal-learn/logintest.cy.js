fd751d98cee15225654626fe40a0d9ecf7442dfc
mansori <mansorihack@gmail.com>
2023-04-23 11:21:14 +0700

    Penambahan custome command

--- cypress/e2e/0-Personal-learn/logintest.cy.js
+++ cypress/e2e/0-Personal-learn/logintest.cy.js
@@ -17,15 +17,7 @@ describe('Working with inputs', () => {
       cy.fixture("user").then(user => { 
         const username = user.username
         const password = user.password
-
-        cy.get('#user_login').clear()
-        cy.get('#user_login').type(username)
-
-        cy.get('input[name="user_password"]').clear()
-        cy.get('input[name="user_password"]').type(password)
-
-        cy.get('input[name="submit"]').click()
-
+        cy.Login(username, password)
         cy.get('.alert-error').should('contain.text', 'Login and/or password are wrong.')
       })
     });
